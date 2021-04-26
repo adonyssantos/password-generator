05ad1592e3f7dd131f3d314113c0779e9cc5a6ee
Adonys Santos <adonysmundotech@gmail.com>
2021-04-26 09:41:54 -0400

    Update index.html

--- js/index.js
+++ js/index.js
@@ -1,9 +1,28 @@
-let twoWords = "",
-  number = 0,
-  symbol = "",
-  website = "",
-  firtLetter = false,
-  lastUpdate = new Date();
+// Get data from index.js
+const getData = () => {
+  let data = [
+    (twoWords = document.getElementById("two-words").value),
+    (number = document.getElementById("number").value),
+    (symbol = document.getElementById("symbol").value),
+    (website = document.getElementById("website").value),
+    (firtLetter = document.getElementById("first-letter__website").value),
+    (lastUpdate = new Date()),
+  ];
 
+  return data;
+};
 
-  
+const get = () => {
+	let input = document.getElementsByTagName("input");
+	for (i = 0; i < input.length; i++) {
+		input[i].addEventListener("change", () => {
+			resultados = getData();
+			console.log(resultados);
+		});
+	}
+};
+
+const submit = document.getElementById("submit");
+submit.addEventListener("click", () => {
+  document.write('fsdfsf')
+});
