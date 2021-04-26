e0e84400a25d8390343afe60ea2d2355629449d5
Adonys Santos <adonysmundotech@gmail.com>
2021-04-26 09:46:41 -0400

    Create generatePassword.js

---
+++ js/generatePassword.js
@@ -0,0 +1,12 @@
+const generatePassword = () => {
+  let twoWords = "",
+  number = 0,
+  symbol = "",
+  website = "",
+  firtLetter = false,
+  lastUpdate = new Date();
+
+  return null;
+}
+
+export default generatePassword;
