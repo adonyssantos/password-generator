0a7488a943ebe88d0fc4273fcc58a26f8fb224aa
Adonys Santos <adonysmundotech@gmail.com>
2021-04-26 10:57:39 -0400

    Declaring variables

--- js/index.js
+++ js/index.js
@@ -18,8 +18,17 @@ const getData = () => {
 };
 
 const generatePassword = (data) => {
-  console.log(data);
-  return data;
+  let twoWords = data[0],
+    number = data[1],
+    symbol = data[2],
+    website = data[3],
+    firtLetter = data[4],
+    currenDate = new Date();
+  let password = "";
+
+  //Generate password code...
+
+  console.log(twoWords, number, symbol, website, firtLetter, currenDate);
 };
 
 const postData = () => {
